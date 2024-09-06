b7363b1442c7125a85b07440a20780b15ab3e038
LeticiaCuero <95148323+LeticiaCuero@users.noreply.github.com>
2024-09-06 14:08:34 -0300

    Calculadora Java

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main (String[] args){
+        double primeiroValor = 0;
+        double segundoValor = 0;
+        int operacao = 0;
+        double resultado = 0;
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Digite o primeiro valor: ");
+        primeiroValor = scn.nextDouble();
+        System.out.println("Selecione a operação: \n 1 - adição \n 2 - subtração \n 3 - multiplicação \n 4 - divisão");
+        operacao = scn.nextInt();
+        System.out.println("Digite o segundo valor: ");
+        segundoValor = scn.nextDouble();
+        if (operacao == 1 ){
+            resultado = primeiroValor + segundoValor;
+        }
+        if (operacao == 2){
+            resultado = primeiroValor - segundoValor;
+        }
+        if (operacao == 3){
+            resultado = primeiroValor * segundoValor;
+
+        }
+        if (operacao == 4){
+            resultado = primeiroValor / segundoValor;
+        }
+        System.out.println("resultado: " + resultado);
+    }
+}
