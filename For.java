ca0f4cd8016bcfb6e0046814d982c9ad1e896ebb
Leticia Cuero <95148323+LeticiaCuero@users.noreply.github.com>
2024-10-03 21:24:00 -0300

    Create For.java

---
+++ For.java
@@ -0,0 +1,12 @@
+public class For {
+    public static void main(String[] args){
+        for(int cont = 1; cont <= 10; cont +=2){
+            if(cont % 2 == 0){
+            System.out.println(cont);
+            }
+        }
+
+
+    }
+}
+
